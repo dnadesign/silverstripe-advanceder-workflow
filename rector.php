2dbb325213b37f99afafcf96b666e949aa01a129
Cameron Bryers <12287346+Cambis@users.noreply.github.com>
2026-01-13 13:16:33 +1300

    :bug: fix: address missing cms6 changes (#1)

---
+++ rector.php
@@ -0,0 +1,45 @@
+<?php
+
+declare(strict_types=1);
+
+use Cambis\SilverstripeRector\Set\ValueObject\SilverstripeLevelSetList;
+use Cambis\SilverstripeRector\Set\ValueObject\SilverstripeSetList;
+use Cambis\SilverstripeRector\Silverstripe413\Rector\Class_\AddBelongsToPropertyAndMethodAnnotationsToDataObjectRector;
+use Cambis\SilverstripeRector\Silverstripe413\Rector\Class_\AddDBFieldPropertyAnnotationsToDataObjectRector;
+use Cambis\SilverstripeRector\Silverstripe413\Rector\Class_\AddExtensionMixinAnnotationsToExtensibleRector;
+use Cambis\SilverstripeRector\Silverstripe413\Rector\Class_\AddHasOnePropertyAndMethodAnnotationsToDataObjectRector;
+use Cambis\SilverstripeRector\Silverstripe52\Rector\Class_\AddBelongsManyManyMethodAnnotationsToDataObjectRector;
+use Cambis\SilverstripeRector\Silverstripe52\Rector\Class_\AddExtendsAnnotationToContentControllerRector;
+use Cambis\SilverstripeRector\Silverstripe52\Rector\Class_\AddExtendsAnnotationToExtensionRector;
+use Cambis\SilverstripeRector\Silverstripe52\Rector\Class_\AddHasManyMethodAnnotationsToDataObjectRector;
+use Cambis\SilverstripeRector\Silverstripe52\Rector\Class_\AddManyManyMethodAnnotationsToDataObjectRector;
+use Rector\Config\RectorConfig;
+use Rector\Php55\Rector\String_\StringClassNameToClassConstantRector;
+use Rector\Php74\Rector\Closure\ClosureToArrowFunctionRector;
+use Rector\Php83\Rector\ClassMethod\AddOverrideAttributeToOverriddenMethodsRector;
+
+return RectorConfig::configure()
+    ->withImportNames(removeUnusedImports: true)
+    ->withPaths([
+        __DIR__ . '/src',
+    ])
+    ->withPhpSets()
+    ->withSets([
+        SilverstripeLevelSetList::UP_TO_SILVERSTRIPE_60,
+    ])
+    ->withSkip([
+        AddOverrideAttributeToOverriddenMethodsRector::class,
+        ClosureToArrowFunctionRector::class,
+        // Don't run this rule on the config itself
+        StringClassNameToClassConstantRector::class => __DIR__ . '/rector.php',
+        // Skip the add annotation rectors
+        AddDBFieldPropertyAnnotationsToDataObjectRector::class,
+        AddBelongsToPropertyAndMethodAnnotationsToDataObjectRector::class,
+        AddHasOnePropertyAndMethodAnnotationsToDataObjectRector::class,
+        AddHasManyMethodAnnotationsToDataObjectRector::class,
+        AddBelongsManyManyMethodAnnotationsToDataObjectRector::class,
+        AddManyManyMethodAnnotationsToDataObjectRector::class,
+        AddExtensionMixinAnnotationsToExtensibleRector::class,
+        AddExtendsAnnotationToContentControllerRector::class,
+        AddExtendsAnnotationToExtensionRector::class,
+    ]);
